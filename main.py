8f79309b2178daf93754d7b2d4fd78b8aacac054
Greg Tracy <gtracy@gmail.com>
2011-08-08 10:09:04 -0500

    cleaned up inadvertent code that managed the open ID login process. there were two files with different pieces in each file being called. i consolidated into just one openid_login.py

--- main.py
+++ main.py
@@ -12,51 +12,18 @@ from dataModel import *
 from twilio.util import TwilioCapability
 from twilio import twiml, TwilioRestException
 from twilio.rest import TwilioRestClient
-import open_id_user
+import openid_login
 
 import configuration
 
-openIdProviders = (
-    'Google.com/accounts/o8/id', # shorter alternative: "Gmail.com"
-    'Yahoo.com',
-    'AOL.com',
-    # add more here
-)
-
-# note that these keynames must match the provider names in front
-# of the .com names above
-buttons = {'Google':'/img/googleButton.png',
-           'Yahoo':'/img/yahooButton.png',
-           'AOL':'/img/aolButton.png',
-          }
-
-class MainHandler(open_id_user.BaseHandler):
+class MainHandler(openid_login.BaseHandler):
     def get(self):
       # force the user to login and get their user model
       user = self.current_user
       if user:
           login = ('%s (<a href="%s">sign out</a>)' % (user.nickname, users.create_logout_url("/")))
       else:
-          greeting = ''
-          results = []
-          for p in openIdProviders:
-              p_name = p.split('.')[0] # take "AOL" from "AOL.com"
-              p_url = p.lower()        # "AOL.com" -> "aol.com"
-              logging.debug('name %s, url %s' % (p_name,p_url))
-              results.append({'name':p_name,
-                              'url':users.create_login_url(federated_identity=p_url,dest_url="/"),
-                              'image':buttons[p_name],
-                              })
-
-          # generate the html
-          template_values = {'greeting':greeting,
-                             'endpoints':results,
-                             'app_title':configuration.APP_TITLE,
-                             'app_description':configuration.APP_DESCRIPTION,
-                             'owner_name':configuration.MY_NAME,
-                             }
-          path = os.path.join(os.path.dirname(__file__), 'templates/splash.html')
-          self.response.out.write(template.render(path, template_values))
+          self.response.out.write(self.user_login_page)
           return
        
       # check the site to determine if the site has been authorized. 
@@ -194,7 +161,7 @@ class VoicemailCallbackHandler(webapp.RequestHandler):
           
           # send the user an SMS message to let them know about the message
           
-class VerifyUserHandler(open_id_user.BaseHandler):
+class VerifyUserHandler(openid_login.BaseHandler):
     def post(self):
         # force the user to login and get their user model
         user = self.current_user
--- openid_login.py
+++ openid_login.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
-
 import os
 import logging
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+
+import configuration
+from dataModel import User
 
 openIdProviders = (
     'Google.com/accounts/o8/id', # shorter alternative: "Gmail.com"
@@ -20,29 +22,51 @@ buttons = {'Google':'/img/googleButton.png',
            'Yahoo':'/img/yahooButton.png',
            'AOL':'/img/aolButton.png',
           }
-class OpenIdLoginHandler(webapp.RequestHandler):
-  def get(self):
-      results = []
-      for p in openIdProviders:
-          p_name = p.split('.')[0] # take "AOL" from "AOL.com"
-          p_url = p.lower()        # "AOL.com" -> "aol.com"
-          results.append({'name':p_name,
-                          'url':users.create_login_url(federated_identity=p_url),
-                          'image':buttons[p_name],
-                          })
-          #self.response.out.write('[<a href="%s">%s</a>]' % 
-          #                        (users.create_login_url(federated_identity=p_url), p_name))
+          
 
-      template_values = {'endpoints':results}
-      path = os.path.join(os.path.dirname(__file__), 'openid-login.html')
-      self.response.out.write(template.render(path, template_values))
+class BaseHandler(webapp.RequestHandler):
+    """ 
+    Base class to help manage user login
+    """
+    @property
+    def current_user(self):
+        if users.get_current_user():
+            user = users.get_current_user()
+            localUser = db.GqlQuery("select * from User where userID = :1", user.user_id()).get()
+            if not localUser:
+                user = User(userID=user.user_id(),
+                            nickname=user.nickname(),
+                            activated=False)
+                user.put()
+                self.redirect('/') #user/edit?userKey=%s' % user.key())
+            else:
+                user = localUser
+            self._current_user = user
+        else:
+            self._current_user = None
 
-def main():
-    application = webapp.WSGIApplication([('/_ah/login_required', OpenIdLoginHandler),
-                                         ],
-                                         debug=True)
-    util.run_wsgi_app(application)
+        return self._current_user
 
+    @property
+    def user_login_page(self):
+          greeting = ''
+          results = []
+          for p in openIdProviders:
+              p_name = p.split('.')[0] # take "AOL" from "AOL.com"
+              p_url = p.lower()        # "AOL.com" -> "aol.com"
+              logging.debug('name %s, url %s' % (p_name,p_url))
+              results.append({'name':p_name,
+                              'url':users.create_login_url(federated_identity=p_url,dest_url="/"),
+                              'image':buttons[p_name],
+                              })
 
-if __name__ == '__main__':
-    main()
+          # generate the html
+          template_values = {'greeting':greeting,
+                             'endpoints':results,
+                             'app_title':configuration.APP_TITLE,
+                             'app_description':configuration.APP_DESCRIPTION,
+                             'owner_name':configuration.MY_NAME,
+                             }
+          path = os.path.join(os.path.dirname(__file__), 'templates/splash.html')
+          return(template.render(path, template_values))
+
